3498d91d33e538b55b90671cce712eac95c33aaf
AkifSivas58 <148585596+AkifSivas58@users.noreply.github.com>
2024-06-06 07:03:47 +0300

    Time: 167 ms (71.51%), Space: 14 MB (54.65%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -1,28 +1,19 @@
+from collections import Counter
 class Solution(object):
     def isNStraightHand(self, hand, groupSize):
         if len(hand) % groupSize != 0:
             return False
         
-        occurences = {}
-        for num in hand:
-            if num not in occurences:
-                occurences[num] = 1
-            else:
-                occurences[num] += 1
+        occurences = Counter(hand)
 
-        hand.sort()
-        while len(hand) != 0:
-            num = hand.pop(0)
-            occurences[num] -= 1
-
-            for i in range(1, groupSize):
-                if num+i in occurences and occurences[num+i] != 0:
-                    occurences[num+i] -= 1
-                    hand.remove(num+i)
-                else:
-                    return False
+        sorted_keys = sorted(occurences.keys())
+        for key in sorted_keys:
+            while occurences[key] > 0:
+                for i in range(groupSize):
+                    if occurences[key + i] > 0:
+                        occurences[key + i] -= 1
+                    else:
+                        return False
 
         return True
-
-
         
