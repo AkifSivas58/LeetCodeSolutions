64181ffdcde1b23fc9e0f330cbf95f2d84249b34
AkifSivas58 <148585596+AkifSivas58@users.noreply.github.com>
2024-08-03 11:39:05 +0300

    Time: 58 ms (37.24%), Space: 11.8 MB (75.17%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.py
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.py
@@ -1,18 +1,10 @@
-from collections import defaultdict
+
 class Solution(object):
     def canBeEqual(self, target, arr):
+
+        arr.sort()
+        target.sort()
+
         if arr == target:
             return True
-        
-        nums = defaultdict(int)
-
-        for num in arr:
-            nums[num] += 1
-        
-        for num in target:
-            if nums[num] == 0:
-                return False
-            else:
-                nums[num] -= 1
-
-        return True
+        return False
